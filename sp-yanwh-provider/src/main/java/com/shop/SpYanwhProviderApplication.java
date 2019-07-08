39d3afced37cf60271a908b9c9beffbe607aef43
wangbin <a395zxb@qq.com>
2019-07-08 10:35:39 +0800

    Initial commit

---
+++ sp-yanwh-provider/src/main/java/com/shop/SpYanwhProviderApplication.java
@@ -0,0 +1,13 @@
+package com.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpYanwhProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpYanwhProviderApplication.class, args);
+    }
+
+}
